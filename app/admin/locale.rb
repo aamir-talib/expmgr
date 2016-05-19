6b4909b065c324fa9cbd611a6c2bcf49277407fa
Prelang Builder <builder@prelang.com>
2016-05-19 18:00:35 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Locale' to allow for create, update

--- app/admin/locale.rb
+++ app/admin/locale.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Locale do
+  permit_params :code, :name, :icon_class, :right_to_left, :position
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
